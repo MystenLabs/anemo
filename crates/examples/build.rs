f85f56b9543f1e8ac15c660d27e3263ad3dc4a2a
Andrew Schran <aschran@mystenlabs.com>
2023-04-24 16:13:28 -0700

    Add support for server handlers returning serialized raw bytes
    - Adds an `IdentityCodec`
    - Adds a `server_handler_use_raw_bytes` option to code generation

--- crates/examples/build.rs
+++ crates/examples/build.rs
@@ -10,6 +10,7 @@ fn main() {
                 .response_type("crate::HelloResponse")
                 .codec_path("anemo::rpc::codec::BincodeCodec")
                 // .codec_path("anemo::rpc::codec::JsonCodec")
+                .server_handler_use_raw_bytes(true)
                 .build(),
         )
         .build();
